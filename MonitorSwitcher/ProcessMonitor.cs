ec9e04f3d3bdc39e73c6e8e58d61feaac1a43650
momre <momre@MNM-PC>
2025-08-04 03:22:38 -0400

    Add project files.

---
+++ MonitorSwitcher/ProcessMonitor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace MonitorSwitcher
+{
+    public class ProcessMonitor
+    {
+        public ManagementEventWatcher _startWatcher;
+        public ManagementEventWatcher _stopWatcher;
+
+        public event EventHandler<ProcessEventArgs> ProcessStarted;
+        public event EventHandler<ProcessEventArgs> ProcessStopped;
+
+    public void StartMonitoring()
+    {
+        _startWatcher = new ManagementEventWatcher();
+        _stopWatcher = new ManagementEventWatcher();
+        var startQuery = new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace");
+
+
+        }
+
+    public void StopMonitoring()
+        {
+            if(_startWatcher != null)
+            {
+                _startWatcher.Stop();
+                _startWatcher.Dispose();
+                _startWatcher = null;
+            }
+            if(_stopWatcher != null)
+            {
+                _stopWatcher.Stop();
+                _stopWatcher.Dispose();
+                _stopWatcher = null;
+            }
+
+            Console.WriteLine("Monitoring stopped.");
+
+        }
+    public class ProcessEventArgs : EventArgs
+{
+    public string ProcessName { get; }
+    public int ProcessId { get; }
+
+    public ProcessEventArgs(string processName, int processId)
+    {
+        ProcessName = processName;
+        ProcessId = processId;
+    }
+}
+}
