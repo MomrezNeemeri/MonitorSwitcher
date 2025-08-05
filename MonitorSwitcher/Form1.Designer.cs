84c6ceecffdc61b139a28d28de1ec8017ecf9d52
momre <momre@MNM-PC>
2025-08-04 20:50:51 -0400

    configured the configuration manager to notify any changes in the processes added in the list

--- MonitorSwitcher/Form1.Designer.cs
+++ MonitorSwitcher/Form1.Designer.cs
@@ -28,22 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             outputListBox = new ListBox();
             tableLayoutPanel1 = new TableLayoutPanel();
-            loadConfigurationbutton = new Button();
-            saveConfigurationButton = new Button();
-            gameListBox = new ListBox();
-            removeGameButton = new Button();
-            addGameButton = new Button();
-            gameNameTextBox = new TextBox();
-            label1 = new Label();
             flowLayoutPanel1 = new FlowLayoutPanel();
+            label1 = new Label();
+            gameNameTextBox = new TextBox();
+            addGameButton = new Button();
+            removeGameButton = new Button();
+            gameListBox = new ListBox();
+            saveConfigurationButton = new Button();
+            loadConfigurationbutton = new Button();
+            notifyIconApp = new NotifyIcon(components);
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            showToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
             tableLayoutPanel1.SuspendLayout();
             flowLayoutPanel1.SuspendLayout();
+            contextMenuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // outputListBox
             // 
+            outputListBox.Dock = DockStyle.Fill;
             outputListBox.FormattingEnabled = true;
             outputListBox.Location = new Point(471, 3);
             outputListBox.Name = "outputListBox";
@@ -57,67 +65,29 @@
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 0, 0);
             tableLayoutPanel1.Controls.Add(outputListBox, 1, 0);
-            tableLayoutPanel1.Location = new Point(171, 44);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
             tableLayoutPanel1.Size = new Size(937, 542);
             tableLayoutPanel1.TabIndex = 1;
             // 
-            // loadConfigurationbutton
+            // flowLayoutPanel1
             // 
-            loadConfigurationbutton.Location = new Point(3, 471);
-            loadConfigurationbutton.Name = "loadConfigurationbutton";
-            loadConfigurationbutton.Size = new Size(125, 29);
-            loadConfigurationbutton.TabIndex = 5;
-            loadConfigurationbutton.Text = "Load Games";
-            loadConfigurationbutton.UseVisualStyleBackColor = true;
-            loadConfigurationbutton.Click += button2_Click;
-            // 
-            // saveConfigurationButton
-            // 
-            saveConfigurationButton.Location = new Point(3, 436);
-            saveConfigurationButton.Name = "saveConfigurationButton";
-            saveConfigurationButton.Size = new Size(140, 29);
-            saveConfigurationButton.TabIndex = 4;
-            saveConfigurationButton.Text = "Save Games";
-            saveConfigurationButton.UseVisualStyleBackColor = true;
-            // 
-            // gameListBox
-            // 
-            gameListBox.Dock = DockStyle.Fill;
-            gameListBox.FormattingEnabled = true;
-            gameListBox.Location = new Point(3, 126);
-            gameListBox.Name = "gameListBox";
-            gameListBox.Size = new Size(445, 304);
-            gameListBox.TabIndex = 3;
-            // 
-            // removeGameButton
-            // 
-            removeGameButton.Location = new Point(3, 91);
-            removeGameButton.Name = "removeGameButton";
-            removeGameButton.Size = new Size(145, 29);
-            removeGameButton.TabIndex = 2;
-            removeGameButton.Text = "Remove Selected";
-            removeGameButton.UseVisualStyleBackColor = true;
-            // 
-            // addGameButton
-            // 
-            addGameButton.Location = new Point(3, 56);
-            addGameButton.Name = "addGameButton";
-            addGameButton.Size = new Size(94, 29);
-            addGameButton.TabIndex = 1;
-            addGameButton.Text = "Add Games";
-            addGameButton.UseVisualStyleBackColor = true;
-            addGameButton.Click += button1_Click;
-            // 
-            // gameNameTextBox
-            // 
-            gameNameTextBox.Location = new Point(3, 23);
-            gameNameTextBox.Name = "gameNameTextBox";
-            gameNameTextBox.Size = new Size(445, 27);
-            gameNameTextBox.TabIndex = 0;
-            gameNameTextBox.TextChanged += gameNameTextBox_TextChanged;
+            flowLayoutPanel1.Controls.Add(label1);
+            flowLayoutPanel1.Controls.Add(gameNameTextBox);
+            flowLayoutPanel1.Controls.Add(addGameButton);
+            flowLayoutPanel1.Controls.Add(removeGameButton);
+            flowLayoutPanel1.Controls.Add(gameListBox);
+            flowLayoutPanel1.Controls.Add(saveConfigurationButton);
+            flowLayoutPanel1.Controls.Add(loadConfigurationbutton);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel1.Location = new Point(3, 3);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(462, 536);
+            flowLayoutPanel1.TabIndex = 1;
             // 
             // label1
             // 
@@ -128,47 +98,120 @@
             label1.TabIndex = 6;
             label1.Text = "Game Process Name:";
             // 
-            // flowLayoutPanel1
+            // gameNameTextBox
             // 
-            flowLayoutPanel1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-            flowLayoutPanel1.Controls.Add(label1);
-            flowLayoutPanel1.Controls.Add(gameNameTextBox);
-            flowLayoutPanel1.Controls.Add(addGameButton);
-            flowLayoutPanel1.Controls.Add(removeGameButton);
-            flowLayoutPanel1.Controls.Add(gameListBox);
-            flowLayoutPanel1.Controls.Add(saveConfigurationButton);
-            flowLayoutPanel1.Controls.Add(loadConfigurationbutton);
-            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
-            flowLayoutPanel1.Location = new Point(3, 3);
-            flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(462, 536);
-            flowLayoutPanel1.TabIndex = 1;
+            gameNameTextBox.Location = new Point(3, 23);
+            gameNameTextBox.Name = "gameNameTextBox";
+            gameNameTextBox.Size = new Size(445, 27);
+            gameNameTextBox.TabIndex = 0;
+            // 
+            // addGameButton
+            // 
+            addGameButton.Location = new Point(3, 56);
+            addGameButton.Name = "addGameButton";
+            addGameButton.Size = new Size(94, 29);
+            addGameButton.TabIndex = 1;
+            addGameButton.Text = "Add Games";
+            addGameButton.UseVisualStyleBackColor = true;
+            addGameButton.Click += AddGameButton_Click;
+            // 
+            // removeGameButton
+            // 
+            removeGameButton.Location = new Point(3, 91);
+            removeGameButton.Name = "removeGameButton";
+            removeGameButton.Size = new Size(145, 29);
+            removeGameButton.TabIndex = 2;
+            removeGameButton.Text = "Remove Selected";
+            removeGameButton.UseVisualStyleBackColor = true;
+            removeGameButton.Click += RemoveGameButton_Click;
+            // 
+            // gameListBox
+            // 
+            gameListBox.FormattingEnabled = true;
+            gameListBox.Location = new Point(3, 126);
+            gameListBox.Name = "gameListBox";
+            gameListBox.Size = new Size(445, 304);
+            gameListBox.TabIndex = 3;
+            // 
+            // saveConfigurationButton
+            // 
+            saveConfigurationButton.Location = new Point(3, 436);
+            saveConfigurationButton.Name = "saveConfigurationButton";
+            saveConfigurationButton.Size = new Size(140, 29);
+            saveConfigurationButton.TabIndex = 4;
+            saveConfigurationButton.Text = "Save Games";
+            saveConfigurationButton.UseVisualStyleBackColor = true;
+            saveConfigurationButton.Click += SaveConfigButton_Click;
+            // 
+            // loadConfigurationbutton
+            // 
+            loadConfigurationbutton.Location = new Point(3, 471);
+            loadConfigurationbutton.Name = "loadConfigurationbutton";
+            loadConfigurationbutton.Size = new Size(125, 29);
+            loadConfigurationbutton.TabIndex = 5;
+            loadConfigurationbutton.Text = "Load Games";
+            loadConfigurationbutton.UseVisualStyleBackColor = true;
+            loadConfigurationbutton.Click += LoadConfigButton_Click;
+            // 
+            // notifyIconApp
+            // 
+            notifyIconApp.ContextMenuStrip = contextMenuStrip1;
+            notifyIconApp.Icon = (Icon)resources.GetObject("notifyIconApp.Icon");
+            notifyIconApp.Text = "Monitor Switcher";
+            notifyIconApp.Visible = true;
+            notifyIconApp.MouseDoubleClick += notifyIconApp_MouseDoubleClick;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { showToolStripMenuItem, exitToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(115, 52);
+            // 
+            // showToolStripMenuItem
+            // 
+            showToolStripMenuItem.Name = "showToolStripMenuItem";
+            showToolStripMenuItem.Size = new Size(114, 24);
+            showToolStripMenuItem.Text = "Show";
+            showToolStripMenuItem.Click += showToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(114, 24);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1210, 647);
+            ClientSize = new Size(937, 542);
             Controls.Add(tableLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
             tableLayoutPanel1.ResumeLayout(false);
             flowLayoutPanel1.ResumeLayout(false);
             flowLayoutPanel1.PerformLayout();
+            contextMenuStrip1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private ListBox outputListBox;
-        private TableLayoutPanel tableLayoutPanel1;
-        private FlowLayoutPanel flowLayoutPanel1;
-        private Label label1;
-        private TextBox gameNameTextBox;
-        private Button addGameButton;
-        private Button removeGameButton;
-        private ListBox gameListBox;
-        private Button saveConfigurationButton;
-        private Button loadConfigurationbutton;
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox gameNameTextBox;
+        private System.Windows.Forms.Button addGameButton;
+        private System.Windows.Forms.Button removeGameButton;
+        private System.Windows.Forms.ListBox gameListBox;
+        private System.Windows.Forms.Button saveConfigurationButton;
+        private System.Windows.Forms.Button loadConfigurationbutton;
+        private System.Windows.Forms.NotifyIcon notifyIconApp;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
--- MonitorSwitcher/Form1.cs
+++ MonitorSwitcher/Form1.cs
@@ -14,14 +14,27 @@ namespace MonitorSwitcher
     public partial class Form1 : Form
     {
         private ProcessMonitor _processMonitor;
+
+        //configuration manager instance
+        private ConfigurationManager _configManager;
+        private List<string> _configuredPrograms;
         public Form1()
         {
             InitializeComponent();
+            //init the ConfigurationManager
+            _configManager = new ConfigurationManager();
+            _configuredPrograms = new List<string>();
+
             //init the ProcessMonitor
             _processMonitor = new ProcessMonitor();
 
             _processMonitor.ProcessStarted += ProcessMonitor_ProcessStarted;
             _processMonitor.ProcessStopped += ProcessMonitor_ProcessStopped;
+
+            loadConfigurationbutton.Click += LoadConfigButton_Click;
+            saveConfigurationButton.Click += SaveConfigButton_Click;
+            removeGameButton.Click += RemoveGameButton_Click;
+            addGameButton.Click += AddGameButton_Click;
 
             // Start monitoring when the form loads
             this.Load += Form1_Load;
@@ -31,7 +44,7 @@ namespace MonitorSwitcher
 
 
         }
-        private void ProcessMonitor_ProcessStarted(object sender, ProcessEventArgs e)
+/*        private void ProcessMonitor_ProcessStarted(object sender, ProcessEventArgs e)
         {
             // Checking if we are currently on a different thread than the UI thread.
             if (this.InvokeRequired)
@@ -58,16 +71,35 @@ namespace MonitorSwitcher
             string message = $"{DateTime.Now.ToLongTimeString()} - Process ENDED: {e.ProcessName} (PID: {e.ProcessId})";
             AppendOutput(message);
 
-        }
-        private void Form1_Load(object sender, EventArgs e)
+        }*/
+        private async void Form1_Load(object sender, EventArgs e)
         {
+            // Load programs automatically when the form loads.
+            _configuredPrograms = await _configManager.LoadConfigAsync();
+            RefreshProcessListUI();
+
+            // Start monitoring processes.
             _processMonitor.StartMonitoring();
+            AppendOutput("Application started. Monitoring processes...");
         }
+
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            _processMonitor.StopMonitoring();
-            AppendOutput("Application closing. Monitoring stopped.");
+            // Logic for hiding the app to the system tray.
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true; // Cancel the close operation.
+                this.Hide();     // Hide the form.
+                notifyIconApp.Visible = true; // Make the tray icon visible.
+                AppendOutput("Application minimized to tray.");
+            }
+            // For a final close, we'll let it happen.
+            else
+            {
+                _processMonitor.StopMonitoring();
+                notifyIconApp.Visible = false;
+            }
         }
 
         // Append the message to the output TextBox
@@ -83,19 +115,123 @@ namespace MonitorSwitcher
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void AddGameButton_Click(object sender, EventArgs e)
         {
+            string processName = gameNameTextBox.Text.Trim();
+            //validate the process name
+            if (string.IsNullOrEmpty(processName))
+            {
+                MessageBox.Show("Please enter a valid process name.");
+                return;
+            }
+            if ((_configuredPrograms.Contains(processName, StringComparer.OrdinalIgnoreCase)))
+            {
+                MessageBox.Show("already in the list", "duplicate entry", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            //add to list and ui
+            _configuredPrograms.Add(processName);
+            gameListBox.Items.Add(processName);
+            gameNameTextBox.Clear();
+            //save list to file
+            await _configManager.SaveConfigAsync(_configuredPrograms);
+            AppendOutput($"Added game: {processName}");
+        }
+        private async void RemoveGameButton_Click(object sender, EventArgs e)
+        {
+            if (gameListBox.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a game to remove.");
+                return;
+            }
+            string selectedProcess = gameListBox.SelectedItem.ToString();
+            _configuredPrograms.Remove(selectedProcess);
+            gameListBox.Items.Remove(selectedProcess);
 
+            await _configManager.SaveConfigAsync(_configuredPrograms);
+            AppendOutput($"Removed game: {selectedProcess}");
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void RefreshProcessListUI()
         {
-
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(RefreshProcessListUI));
+                return;
+            }
+            gameListBox.Items.Clear();
+            foreach (var game in _configuredPrograms)
+            {
+                gameListBox.Items.Add(game);
+            }
+        }
+        private async void SaveConfigButton_Click(object sender, EventArgs e)
+        {
+            await _configManager.SaveConfigAsync(_configuredPrograms);
+            AppendOutput("Configuration manually saved.");
         }
 
-        private void gameNameTextBox_TextChanged(object sender, EventArgs e)
+        private async void LoadConfigButton_Click(object sender, EventArgs e)
         {
+            _configuredPrograms = await _configManager.LoadConfigAsync();
+            RefreshProcessListUI();
+            AppendOutput("Configuration manually loaded.");
+        }
+        // process moinitoring event handlers
 
+        private void ProcessMonitor_ProcessStarted(object sender, ProcessEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => ProcessMonitor_ProcessStarted(sender, e)));
+                return;
+            }
+
+            string message = $"{DateTime.Now.ToLongTimeString()} - Process STARTED: {e.ProcessName} (PID: {e.ProcessId})";
+            AppendOutput(message);
+
+            if (_configuredPrograms.Contains(e.ProcessName, StringComparer.OrdinalIgnoreCase))
+            {
+                AppendOutput($"--- Configured PROGRAM STARTED: {e.ProcessName} ---");
+                // TODO: Implement monitor switching logic .
+            }
+        }
+
+        private void ProcessMonitor_ProcessStopped(object sender, ProcessEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => ProcessMonitor_ProcessStopped(sender, e)));
+                return;
+            }
+
+            string message = $"{DateTime.Now.ToLongTimeString()} - Process ENDED: {e.ProcessName} (PID: {e.ProcessId})";
+            AppendOutput(message);
+
+            if (_configuredPrograms.Contains(e.ProcessName, StringComparer.OrdinalIgnoreCase))
+            {
+                AppendOutput($"--- Configured PROGRAM ENDED: {e.ProcessName} ---");
+                // TODO: Implement  monitor reverting logic .
+            }
+        }
+        // --- System Tray Icon Event Handlers ---
+
+        private void notifyIconApp_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIconApp.Visible = false;
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIconApp_MouseDoubleClick(sender, null);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
